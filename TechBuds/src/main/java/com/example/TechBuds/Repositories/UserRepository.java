bb6c810464bf5906e0b0d991f9aab5de03ba1feb
Sachithra Pinnaduwa <136422527+SachithraPinnaduwa@users.noreply.github.com>
2025-03-08 22:20:03 +0530

    fixed user by removing unnessary parts from it

--- TechBuds/src/main/java/com/example/TechBuds/Repositories/UserRepository.java
+++ TechBuds/src/main/java/com/example/TechBuds/Repositories/UserRepository.java
@@ -10,5 +10,4 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
 }
