21336db8dde0eaaeb5356bd08237f10837a3f651
Rukshan Dias <diasrukshan21@gmail.com>
2025-03-08 08:28:08 +0530

    Backend code

---
+++ TechBuds/src/main/java/com/example/TechBuds/TechBudsApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechBuds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechBudsApplication.class, args);
+	}
+
+}
---
+++ TechBuds/src/test/java/com/example/TechBuds/TechBudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechBuds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBudsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
