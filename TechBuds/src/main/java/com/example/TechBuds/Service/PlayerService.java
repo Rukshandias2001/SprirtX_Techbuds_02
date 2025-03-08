6e4b8ce8abad0d49d5f1caca33fab2bc50351dec
Rukshan Dias <diasrukshan21@gmail.com>
2025-03-08 14:57:35 +0530

    Backend LeaderBoard

--- TechBuds/src/main/java/com/example/TechBuds/Service/PlayerService.java
+++ TechBuds/src/main/java/com/example/TechBuds/Service/PlayerService.java
@@ -1,11 +1,12 @@
 package com.example.TechBuds.Service;
 
 import com.example.TechBuds.Entities.PlayerStats;
+import com.example.TechBuds.Modal.PlayerStatDTO;
 
 import java.util.ArrayList;
 
 public interface PlayerService {
-    ArrayList<PlayerStats> getPlayerStats();
+    ArrayList<PlayerStatDTO> getPlayerStats();
 
 
 
